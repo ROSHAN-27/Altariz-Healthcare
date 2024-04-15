ef2cedbe58ec60529625f5dcbefd71a3fb9e4b9b
ROSHAN-27 <86292504+ROSHAN-27@users.noreply.github.com>
2024-04-16 02:23:00 +0530

    second commit

--- javascript1/image.js
+++ javascript1/image.js
@@ -3,8 +3,8 @@ const sliderImg = document.getElementById('imageChange');
 function slider() {
     let i =1;
     setInterval(()=>{        
-            sliderImg.src= `../images/sliderImage${i}.png`
-            console.log(`../images/sliderImage${i}.png`)
+            sliderImg.src= `../images1/sliderImage${i}.png`
+            console.log(`../images1/sliderImage${i}.png`)
             if(i>=7) {
                 i=1;
             } else {
